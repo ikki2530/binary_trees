10e6d1379e2981728a6f9bee2142495539aa645d
Diego Gomez <dagomez2530@gmail.com>
2020-07-21 20:26:59 +0000

    Adding file for task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - deletes a binary tree from root
+ * @tree: root of the binary tree
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
