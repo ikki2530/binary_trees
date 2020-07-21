f739b400759d18ee90eb41a24d2f9f8ebba4c4b6
Diego Gomez <dagomez2530@gmail.com>
2020-07-21 17:40:58 +0000

    Adding file for task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert a node to the left
+ * @parent: parent node of the new node
+ * @value: data of the new node
+ * Return: Address of new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+
+	if (parent == NULL)
+		return (NULL);
+
+	newnode = binary_tree_node(parent, value);
+	if (newnode == NULL)
+		return (NULL);
+
+	if (parent->left == NULL)
+	{
+		parent->left = newnode;
+		newnode->parent = parent;
+	}
+	else
+	{
+		/*connect new node with the left child of parent*/
+		newnode->left = parent->left;
+		parent->left->parent = newnode;
+		/*connect the new node with the parent*/
+		newnode->parent = parent;
+		parent->left = newnode;
+	}
+
+	return (newnode);
+}
